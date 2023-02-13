1c6ac81dd3bcc9ca893511f643d90ff7cfbab6fe
iamhemantgauhai <hemant20@navgurukul.org>
2023-02-13 10:53:19 +0530

    initial commit

--- lib/Widgets/recycler_view.dart
+++ lib/Widgets/recycler_view.dart
@@ -14,44 +14,195 @@ class RecyclerViewData extends StatefulWidget {
 class _RecyclerViewDataState extends State<RecyclerViewData> {
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (ctx) => AlertDialog(
-              title: const Text("Alert Dialog Box"),
-              content: const Text("You have raised a Alert Dialog Box"),
-              actions: <Widget>[
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(ctx).pop();
-                  },
-                  child: Container(
-                    color: Colors.green,
-                    padding: const EdgeInsets.all(14),
-                    child: const Text("okay"),
-                  ),
-                ),
-              ],
+    return Container(
+      padding: const EdgeInsets.only(top: 110.0),
+      color: Colors.white,
+      child: FutureBuilder<List<Job>>(
+        future: _fetchJobs(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<Job>? data = snapshot.data;
+            return ListView.builder(
+                itemCount: data!.length,
+                itemBuilder: (context, index) {
+                  return TextButton(
+                      onPressed: () {
+                        showAlertDialog(BuildContext context) {
+                          Widget cancelButton = TextButton(
+                            child: const Text("Cancel",
+                                style: TextStyle(color: Colors.white)),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          );
+                          Widget continueButton = TextButton(
+                            child: const Text(
+                              "Continue",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          );
+                          AlertDialog alert = AlertDialog(
+                            insetPadding: const EdgeInsets.all(5.0),
+                            scrollable: true,
+                            content: Container(
+                              width: MediaQuery.of(context).size.width,
+                              color: Colors.white,
+                              child: Column(
+                                children: [
+                                  Container(
+                                      width: double.infinity,
+                                      height: 40.0,
+                                      color: Colors.black,
+                                      child: const Center(
+                                          child: Text(
+                                        "Selected Date is : 01/02/23",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 15.0),
+                                      ))),
+                                  Container(
+                                    height: 5.0,
+                                    color: Colors.white,
+                                  ),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                        border: Border.all(
+                                            color: Colors.black, width: 10.0)),
+                                    child: Column(
+                                      children: [
+                                        SizedBox(
+                                          height: 380,
+                                          child: ListView.builder(
+                                              itemCount: 6,
+                                              itemBuilder: (context, i) {
+                                                return Row(
+                                                  children: [
+                                                    Padding(
+                                                      padding:
+                                                          const EdgeInsets.all(
+                                                              18.0),
+                                                      child: Column(
+                                                          crossAxisAlignment:
+                                                              CrossAxisAlignment
+                                                                  .start,
+                                                          children: [
+                                                            const Text(
+                                                                'Dealer Name:'),
+                                                            const Text(
+                                                                'Electronic Paradise'),
+                                                            const Text(''),
+                                                            const Text(
+                                                                'Address : B1 Janakpuri'),
+                                                            const Text(''),
+                                                            Padding(
+                                                              padding:
+                                                                  const EdgeInsets
+                                                                          .only(
+                                                                      left:
+                                                                          40.0),
+                                                              child: ElevatedButton(
+                                                                  onPressed:
+                                                                      () {},
+                                                                  style: const ButtonStyle(
+                                                                      backgroundColor: MaterialStatePropertyAll<
+                                                                              Color>(
+                                                                          Colors
+                                                                              .grey)),
+                                                                  child: const Text(
+                                                                      'Edit')),
+                                                            ),
+                                                          ]),
+                                                    ),
+                                                    Column(
+                                                      crossAxisAlignment:
+                                                          CrossAxisAlignment
+                                                              .start,
+                                                      children: [
+                                                        const Text(
+                                                            'Dealer ID:'),
+                                                        const Text('10001'),
+                                                        const Text(''),
+                                                        const Text(''),
+                                                        const Text(''),
+                                                        Padding(
+                                                          padding:
+                                                              const EdgeInsets
+                                                                      .only(
+                                                                  right: 50.0),
+                                                          child: ElevatedButton(
+                                                              style: const ButtonStyle(
+                                                                  backgroundColor:
+                                                                      MaterialStatePropertyAll<
+                                                                              Color>(
+                                                                          Colors
+                                                                              .black)),
+                                                              onPressed: () {},
+                                                              child: const Text(
+                                                                  'Visit')),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                  ],
+                                                );
+                                              }),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 10.0,
+                                    color: Colors.white,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      ElevatedButton(
+                                          onPressed: () {},
+                                          style: const ButtonStyle(
+                                              backgroundColor:
+                                                  MaterialStatePropertyAll<
+                                                      Color>(Colors.black)),
+                                          child: cancelButton),
+                                      ElevatedButton(
+                                          onPressed: () {},
+                                          style: const ButtonStyle(
+                                              backgroundColor:
+                                                  MaterialStatePropertyAll<
+                                                      Color>(Colors.black)),
+                                          child: continueButton),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return alert;
+                            },
+                          );
+                        }
+
+                        showAlertDialog(context);
+                      },
+                      child: _tile(data[index].position, data[index].company,
+                          Icons.work));
+                });
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
+          return const Center(
+            child: CircularProgressIndicator(
+              color: Colors.purple,
             ),
           );
         },
-        child: FutureBuilder<List<Job>>(
-          future: _fetchJobs(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              List<Job>? data = snapshot.data;
-              return _jobsListView(data);
-            } else if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            }
-            return const Center(
-              child: CircularProgressIndicator(
-                color: Colors.purple,
-              ),
-            );
-          },
-        ),
+      ),
     );
   }
 }
@@ -68,44 +219,28 @@ Future<List<Job>> _fetchJobs() async {
   }
 }
 
-Center _jobsListView(data) {
-  return Center(
-    child: ListView.builder(
-        itemCount: data.length,
-        itemBuilder: (context, index) {
-          return _tile(data[index].position, data[index].company, Icons.work);
-        }),
-  );
-}
-
 ListTile _tile(String title, String subtitle, IconData icon) => ListTile(
-      title: Center(
-        child: Container(
-          width: double.infinity,
-          color: Colors.purple,
-          child: TextButton(
-            onPressed: () {},
-            child: Column(
-              children: [
-                Icon(
-                  icon,
-                  color: Colors.white,
-                ),
-                Text(
-                  title,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 20,
-                  ),
-                ),
-                Text(
-                  subtitle,
-                  style: const TextStyle(color: Colors.white),
-                )
-              ],
+      title: Container(
+        color: Colors.purple,
+        child: Column(
+          children: [
+            Icon(
+              icon,
+              color: Colors.white,
             ),
-          ),
+            Text(
+              title,
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+                fontSize: 20,
+              ),
+            ),
+            Text(
+              subtitle,
+              style: const TextStyle(color: Colors.white),
+            )
+          ],
         ),
       ),
     );
